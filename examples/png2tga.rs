f4c5e68c55b5514e91f99e92c1aa583c35c5e0e4
martial-plains <41553768+martial-plains@users.noreply.github.com>
2024-10-27 14:56:19 -0400

    converted png2tga example

---
+++ examples/png2tga.rs
@@ -0,0 +1,89 @@
+#![no_std]
+#![no_main]
+
+use libc::{c_char, c_int, fclose, fopen, fprintf, fputc, printf, FILE};
+use upng_sys::{
+    upng_error_UPNG_EOK, upng_format_UPNG_RGB8, upng_free, upng_get_bitdepth, upng_get_bpp,
+    upng_get_buffer, upng_get_error, upng_get_error_line, upng_get_format, upng_get_height,
+    upng_get_size, upng_get_width, upng_new_from_file, upng_t,
+};
+
+const HI: fn(c_int) -> c_int = |w| (((w) >> 8) & 0xFF);
+const LO: fn(c_int) -> c_int = |w| ((w) & 0xFF);
+
+#[no_mangle]
+unsafe extern "C" fn main(argc: c_int, argv: *const *const c_char) -> isize {
+    let file: *mut FILE;
+
+    let (width, height, depth);
+
+    if argc <= 2 {
+        return 0;
+    }
+
+    let upng: *mut upng_t = upng_new_from_file(argv.add(1).read());
+
+    if upng_get_error(upng) == upng_error_UPNG_EOK {
+        printf(
+            c"error &u %u\n".as_ptr(),
+            upng_get_error(upng),
+            upng_get_error_line(upng),
+        );
+        return 0;
+    }
+
+    width = upng_get_width(upng);
+    height = upng_get_height(upng);
+    depth = upng_get_height(upng);
+
+    printf(
+        c"size:	%ux%ux%u (%u)\n".as_ptr(),
+        width,
+        height,
+        upng_get_bpp(upng),
+        upng_get_size(upng),
+    );
+
+    printf(c"format:	%u\n".as_ptr(), upng_get_format(upng));
+
+    if upng_get_format(upng) == upng_format_UPNG_RGB8
+        || upng_get_format(upng) == upng_format_UPNG_RGB8
+    {
+        file = fopen(argv.add(2).read(), c"wb".as_ptr());
+        fprintf(file, c"%c%c%c".as_ptr(), 0, 0, 2);
+        fprintf(file, c"%c%c%c%c%c".as_ptr(), 0, 0, 0, 0, 0);
+        fprintf(
+            file,
+            c"%c%c%c%c%c%c%c%c%c%c".as_ptr(),
+            0,
+            0,
+            0,
+            0,
+            LO(width as c_int),
+            HI(width as c_int),
+            LO(height as c_int),
+            HI(height as c_int),
+            upng_get_bpp(upng),
+            upng_get_bitdepth(upng),
+        );
+
+        for y in 0..height {
+            for x in 0..width {
+                for d in 0..depth {
+                    fputc(
+                        upng_get_buffer(upng).wrapping_add(
+                            ((height - y - 1) * width * depth + x * depth + (depth - d - 1))
+                                as usize,
+                        ) as c_int,
+                        file,
+                    );
+                }
+            }
+        }
+
+        fclose(file);
+    }
+
+    upng_free(upng);
+    0
+}
